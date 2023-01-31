31acf686bd6e0849c433dffe8ba77acca8ed8c00
tsleesg <114339973+tsleesg@users.noreply.github.com>
2023-01-31 09:15:07 +0100

    Update Log.py

--- main/Log.py
+++ main/Log.py
@@ -1,34 +1,27 @@
-from colorit import *
+from colorit import init_colorit, color, background
 from django.conf import settings
-
 
 init_colorit()
 
+def log(tag, *args, color_func=color, bg_color=None):
+    args = ", ".join([str(item) for item in args])
+    if bg_color:
+        print(background(f"{tag}: {args}", bg_color))
+    else:
+        print(color_func(f"{tag}: {args}", color_func))
 
 def debug(tag, *args):
     if settings.DEBUG:
-        args = ", ".join([str(item) for item in args])
-        print(color(f"{tag}: {args}", Colors.blue))
-
+        log(tag, *args, color_func=Colors.blue)
 
 def error(tag, *args):
-    args = ", ".join([str(item) for item in args])
-    print(color(f"{tag}: {args}", Colors.red))
-
+    log(tag, *args, color_func=Colors.red)
 
 def info(tag, *args):
-    args = ", ".join([str(item) for item in args])
-    print(color(f"{tag}: {args}", Colors.purple))
-
+    log(tag, *args, color_func=Colors.purple)
 
 def message(tag, *args, green=False):
-    args = ", ".join([str(item) for item in args])
-    if green:
-        print(background(f"{tag}: {args}", Colors.green))
-    else:
-        print(background(f"{tag}: {args}", Colors.red))
-
+    log(tag, *args, bg_color=Colors.green if green else Colors.red)
 
 def warning(tag, *args):
-    args = ", ".join([str(item) for item in args])
-    print(color(f"{tag}: {args}", Colors.orange))
+    log(tag, *args, color_func=Colors.orange)
