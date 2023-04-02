5b4ddcaaae6cf09ce4ba67d371e09895a821f25e
sk00935018 <126136453+sk00935018@users.noreply.github.com>
2023-04-02 22:42:47 +0530

    updated story creation file

--- rasa_app/createStory.py
+++ rasa_app/createStory.py
@@ -1,37 +1,50 @@
-import json
 import yaml
-from collections import OrderedDict
+import json
 
-
-def generate_stories_json(json_string):
-    bot_data = json.loads(json_string)
-    
-    stories = []
+def generate_stories_json():
+    with open('payload4.json', 'r') as f:
+        bot_data = json.load(f)
+    intentList = []
+    child=None
     for intent in bot_data['intents']:
-        for i, utterance in enumerate(intent['utterances']):
-            story = f"\n## {intent['name']} {i+1}"
-            story += f"\n- {utterance}"
-            
-            story += f"\n  - {intent['responses'][i]}"
-            
-            if 'child_id' in intent:
-                child_id = intent['child_id']
-                
-                while child_id != '+1':
-                    child_intent = next(child for child in bot_data['intents'] if child['id'] == child_id)
-                    story += f"\n- {child_intent['utterances'][0]}"
-                    child_id = child_intent['child_id']
-                goodbye_intent = next(child for child in bot_data['intents'] if child['id'] == '+1')
-                story += f"\n  - {goodbye_intent['responses'][i]}"
-            
-            stories.append(story)
-    stories_string = "\n".join(stories)
-    return stories_string
+        if(intent["parent_id"]==-1):
+            intentList.append(intent["name"])
+            child=intent["child_id"]
+        for intent in bot_data["intents"]:
+            if(intent["id"]==child):
+                intentList.append(intent["name"])
+                child=intent["child_id"]
+    return intentList
 
 
-with open('payload4.json', 'r') as f:
-    data = json.load(f,object_pairs_hook=OrderedDict)
-new_data=generate_stories_json(data)
-yaml_data = yaml.dump(new_data)
-with open('stories2.yml', 'w') as yaml_file:
-    yaml_file.write(yaml_data)
+
+def generate_stories(intents):
+    story_dict = {
+    "version": "3.0",
+    "stories": [
+        {
+            "story": f"Story path {'-'.join(intents)}"},
+         {   "steps": []
+        }
+            ]
+    }
+    for intent in intents:
+        step_intent = {
+        "intent": intent
+        }
+        step_action={"action":"utter_"+intent}
+        story_dict["stories"][1]["steps"].append(step_intent)
+        story_dict["stories"][1]["steps"].append(step_action)
+
+    with open("tories.yml", "w") as f:
+        yaml.dump(story_dict, f)
+
+intents=generate_stories_json()
+generate_stories(intents)
+
+
+
+
+
+
+
