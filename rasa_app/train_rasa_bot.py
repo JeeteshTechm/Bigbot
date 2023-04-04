e2a371c136bee49eac5136bccae4a72c6a449ac6
tsleesg <tsleeuk@gmail.com>
2023-04-04 12:49:25 +0800

    Update train_rasa_bot.py
    added a helper method and streamlined train_and_save_model()
    
    should add some logging and argument parsing to let users specify the bot name/ID

--- rasa_app/train_rasa_bot.py
+++ rasa_app/train_rasa_bot.py
@@ -4,12 +4,15 @@ class TrainBot:
     def __init__(self, bot_id):
         self.bot_id = bot_id
 
-    def train_and_save_model(self):
+    def _create_file_paths(self):
         config = f"{self.bot_id}/config.yml"
         training_files = f"{self.bot_id}/data/"
         domain = f"{self.bot_id}/domain.yml"
-
         output = f"{self.bot_id}/models/"
+        return config, training_files, domain, output
+
+    def train_and_save_model(self):
+        config, training_files, domain, output = self._create_file_paths()
 
         rasa.train(domain, config, [training_files], output, fixed_model_name='rasa_model')
 
@@ -18,4 +21,3 @@ class TrainBot:
 bot_id = "my_bot"
 bot_trainer = TrainBot(bot_id)
 response = bot_trainer.train_and_save_model()
-
