a1ea4f979a97dac4868e2a6ffbad60f9c78ba270
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-04-05 19:01:49 +0530

    Update nlu and actions

--- rasa_app/rasa_file_generator.py
+++ rasa_app/rasa_file_generator.py
@@ -23,15 +23,24 @@ class RasaFileGenerator:
 
     def create_nlu_file(self):
         nlu_file = os.path.join(self.skill_id, "data", "nlu.yml")
-
         intents = self.payload["intents"]
         yaml_output = "version: '3.1'\n"
         yaml_output += "nlu:\n"
         for intent in intents:
-            yaml_output += f"  - intent: {intent['name']}\n"
-            yaml_output += f"    examples: |\n"
-            for example in intent['utterances']:
-                yaml_output += f"      - {example}\n"
+            if "entities" in intent:
+                yaml_output += f"  - intent: {intent['name']}\n"
+                yaml_output += "    examples: |\n"
+                for example in intent['utterances']:
+                    example_with_entities = example
+                    for entity in intent['entities']:
+                        for value in entity['values']:
+                            example_with_entities = example_with_entities.replace(value, f"[{value}]({entity['name']})")
+                    yaml_output += f"      - {example_with_entities}\n"
+            else:
+                yaml_output += f"  - intent: {intent['name']}\n"
+                yaml_output += "    examples: |\n"
+                for example in intent['utterances']:
+                    yaml_output += f"      - {example}\n"
 
         with open(nlu_file, "w") as yaml_file:
             yaml_file.write(yaml_output)
@@ -81,24 +90,39 @@ class RasaFileGenerator:
         shutil.copyfile(config_file_path, os.path.join(self.skill_id, 'config.yml'))
         return "Config file uploaded successfully"
 
+
     def generate_actions_file(self):
         actions_code = ""
         for intent in self.payload["intents"]:
             if "api_call" in intent:
                 class_name = f"Action{intent['name'].title().replace(' ', '')}"
                 actions_code += f"class {class_name}(Action):\n"
+                
                 actions_code += f"    def name(self) -> Text:\n"
                 actions_code += f"        return 'action_{intent['name']}'\n\n"
+
                 actions_code += f"    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:\n"
                 actions_code += f"        return self._call_api_and_utter_response(dispatcher, tracker, domain)\n\n"
                 actions_code += f"    def _call_api_and_utter_response(self, dispatcher, tracker, domain):\n"
-                actions_code += f"        dispatcher.utter_message(text=response_text)\n\n"
 
                 # check if API call requires request body
-                if "request_body" in intent["api_call"]:
-                    actions_code += f"        request_body = {json.dumps(intent['api_call']['request_body'], indent=2)}\n"
-                else:
-                    actions_code += f"        request_body = None\n"
+                entity_list=[]
+                if "entities" in intent:
+                    for entity in intent["entities"]:
+                        entity_name = entity["name"]
+                        entity_list.append(entity_name)
+                #entity_list = [ast.literal_eval(s) for s in entity_list]
+
+
+                request_body_list=intent["api_call"]["request_body"]
+
+                request_body = dict(zip(request_body_list, entity_list))
+                
+                
+                for key, value in request_body.items():
+                    
+                    actions_code += f"        {value} = tracker.get_slot({value})\n"
+               
 
                 actions_code += f"        response = requests.{intent['api_call']['method']}('{intent['api_call']['url']}', json=request_body)\n"
                 actions_code += f"        response_text = response.json()['{intent['response_field']}']\n"
