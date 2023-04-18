5a3c8adba4db22cad3ab2b0125e035b756035595
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-04-18 19:56:13 +0530

    Updated with actions

--- rasa_app/rasa_inference_agent.py
+++ rasa_app/rasa_inference_agent.py
@@ -1,9 +1,13 @@
-import asyncio
 from rasa.core.agent import Agent
+from rasa.core.interpreter import RasaNLUInterpreter
+from rasa.utils.endpoints import EndpointConfig
+import asyncio
 
 class Bot:
-    def __init__(self, model_path):
-        self.agent = Agent.load(model_path)
+    def __init__(self, model_path, endpoint_url):
+        interpreter = RasaNLUInterpreter(model_path)
+        endpoint = EndpointConfig(url=endpoint_url)
+        self.agent = Agent.load(model_path, interpreter=interpreter, action_endpoint=endpoint)
 
     async def get_response(self, user_message):
         parsed = await self.agent.parse_message(user_message)
@@ -16,7 +20,8 @@ class Bot:
         return result
 
 async def main():
-    bot = Bot("concertbot/models")
+    endpoint_url = "http://localhost:5055/webhook"  # replace with your endpoint URL
+    bot = Bot("path/to/nlu/model", endpoint_url)
     response = await bot.get_response("hi")
     print(response)
 
