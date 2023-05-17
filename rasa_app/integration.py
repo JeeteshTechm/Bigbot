9342d3f48cb199cfb57b1c0a463211f62a5d784e
sk00935018 <126136453+sk00935018@users.noreply.github.com>
2023-05-17 20:47:05 +0530

    Updated working integration.py on agent and delegate mapping

--- rasa_app/integration.py
+++ rasa_app/integration.py
@@ -5,22 +5,23 @@ from rasa.shared.core.domain import Domain
 from rasa.core.tracker_store import DialogueStateTracker
 import asyncio
 
-class Bot:
-
+class skill:
     async def get_response(self, user_message):
-        agent=Agent.load("./models")
+        agent=Agent.load("Skill_finder/models")
         parsed = await agent.parse_message(user_message)
         print("___________________",parsed["text"])
         skill_id=parsed["intent"]["name"]
-        print(skill_id)
-        bot=Bot()
-        sender_id = "sandeep"
-        conv_response= await bot.get_conversation(skill_id,user_message,sender_id)
+        print(type(skill_id))
+        # skill_id="book_restaurant"
+         
+        return skill_id
 
-        # result = { 'intent': parsed} 
-        return conv_response
 
-    async def get_conversation(self,skill_id, user_message, sender_id):
+
+
+class Bot:
+    def __init__(self, model_path, endpoint_url, domain,sender_id):
+        endpoint = EndpointConfig(url=endpoint_url)
         tracker_store = SQLTrackerStore(
                 dialect="postgresql",
                 host="127.0.0.1",
@@ -29,26 +30,32 @@ class Bot:
                 password="rasa123",
                 db="rasadb"
                 )
-        endpoint = "http://37.224.68.171:5055/webhook"
-        model_path= skill_id + "/models"
-        domain=skill_id + "/domain.yml"
-        agent1 = Agent.load(model_path, action_endpoint=endpoint, tracker_store=tracker_store, domain=domain)
-        parsed = await agent1.parse_message(user_message)
-        intent = parsed['intent']['name']
-        confidence = parsed['intent']['confidence']
-        response = await agent1.handle_text(user_message,sender_id=sender_id)
+        #tracker_store = InMemoryTrackerStore(max_event_history=10, domain=domain,sender_id=sender_id)
+        self.agent = Agent.load(model_path, action_endpoint=endpoint, tracker_store=tracker_store, domain=domain)
+
+    async def get_conv(self, user_message, sender_id):
+        parsed = await self.agent.parse_message(user_message)
+        # intent = parsed['intent']['name']
+        # confidence = parsed['intent']['confidence']
+        response = await self.agent.handle_text(user_message,sender_id=sender_id)
+        print("____________________",response)
+        print("-------------------",parsed)
         result = {'text': response, 'intent': parsed}
+       
     
         return result
 
-
-
 async def main():
+    sk=skill()
+    user_message="book table"
     
-    input_message="Book table"
-    bot=Bot()
-    response = await bot.get_response(input_message)
-    print(response)
-    
-   
+    endpoint_url = "http://37.224.68.171:5055/webhook"
+    sender_id = "san"
+    skill_id= await sk.get_response(user_message)
+    print(skill_id)
+    domain = Domain.load(skill_id+"/domain.yml")
+    bot = Bot(skill_id+"/models", endpoint_url, domain,sender_id)
+    response = await bot.get_conv(user_message, sender_id)
+    print("_______-------------------",response)
+
 asyncio.run(main())
