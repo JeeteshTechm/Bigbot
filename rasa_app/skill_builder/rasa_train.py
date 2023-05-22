dbf858deb058e86374f92af737313ed362ef3e97
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-05-22 17:10:31 +0530

    Update rasa_train.py

--- rasa_app/skill_builder/rasa_train.py
+++ rasa_app/skill_builder/rasa_train.py
@@ -1,23 +1,49 @@
+from flask import Flask, jsonify, request
+from rasa_file_generator import RasaFileGenerator
+from actions.run_actions import ActionServerManager
+import json
 import rasa
 
-class TrainBot:
-    def __init__(self, bot_id):
-        self.bot_id = bot_id
+app = Flask(__name__)
 
-    def _create_file_paths(self):
-        config = f"{self.bot_id}/config.yml"
-        training_files = f"{self.bot_id}/data/"
-        domain = f"{self.bot_id}/domain.yml"
-        output = f"{self.bot_id}/models/"
-        return config, training_files, domain, output
+@app.route('/file_generate', methods=['POST'])
+def generate():
+    try:
+        uploaded_file = request.files['file']
+        payload = json.load(uploaded_file)
 
-    def train_and_save_model(self):
-        config, training_files, domain, output = self._create_file_paths()
+        skill_id = str(payload["bot_id"])
+
+        rasa_project = RasaFileGenerator(skill_id, payload)
+        rasa_run_actions = ActionServerManager()
+
+        rasa_project.create_rasa_folder_structure()
+        rasa_project.generate_training_data()
+        rasa_project.generate_domain_file()
+        rasa_project.upload_config_file("input/config.yml")
+        rasa_project.generate_actions_file()
+        rasa_run_actions.start_action_server()
+
+        return jsonify({"message": "Rasa files generated successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/train', methods=['POST'])
+def train():
+    try:
+        data = request.get_json()
+        skill_id = data['skill_id']
+        config = f"skills/{skill_id}/config.yml"
+        training_files = f"skills/{skill_id}/data/"
+        domain = f"skills/{skill_id}/domain.yml"
+        output = f"skills/{skill_id}/models/"
 
         rasa.train(domain, config, [training_files], output, fixed_model_name='rasa_model')
 
-        return "Your Rasa model has been trained and saved"
+        return jsonify({"message": "Rasa model has been trained and saved in models"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-bot_id = "my_bot"
-bot_trainer = TrainBot(bot_id)
-response = bot_trainer.train_and_save_model()
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5005)
+
