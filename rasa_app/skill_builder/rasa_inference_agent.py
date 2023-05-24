33eacea2334935137df0e207b6c30ff3c9c73476
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-05-24 19:35:12 +0530

    Update rasa_inference_agent.py

--- rasa_app/skill_builder/rasa_inference_agent.py
+++ rasa_app/skill_builder/rasa_inference_agent.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from flask import Flask, jsonify, request
 from rasa.core.agent import Agent
@@ -6,8 +7,11 @@ from rasa.core.tracker_store import SQLTrackerStore
 from rasa.shared.core.domain import Domain
 import asyncio
 import yaml
-
 app = Flask(__name__)
+
+log_file = 'logs/agent.log'
+logging.basicConfig(filename=log_file, level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Bot:
     def __init__(self, model_path, config_path, domain_path):
@@ -46,24 +50,29 @@ class Bot:
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    data = request.get_json()
-    user_message = data['message']
-    sender_id = data['sender_id']
-    skill_id = data['skill_id']
+    try:
+        data = request.get_json()
+        user_message = data['message']
+        sender_id = data['sender_id']
+        skill_id = data['skill_id']
 
-    model_path = f"skills/{skill_id}/models/"
-    config_path = "data/db_config.yml"
-    domain_path = f"skills/{skill_id}/domain.yml"
-    domain = Domain.load(domain_path)
+        model_path = f"skills/{skill_id}/models/"
+        config_path = "input/db_config.yml"
+        domain_path = f"skills/{skill_id}/domain.yml"
+        domain = Domain.load(domain_path)
 
-    if not os.path.exists(model_path) or not os.path.isfile(domain_path):
-        return jsonify({'error': 'Invalid skill_id'})
+        if not os.path.exists(model_path) or not os.path.isfile(domain_path):
+            logger.error(f"Invalid skill_id: {skill_id}")
+            return jsonify({'error': 'Invalid skill_id'})
 
-    bot = Bot(model_path, config_path, domain)
-    response = asyncio.run(bot.get_response(user_message, sender_id))
-    return jsonify(response)
+        bot = Bot(model_path, config_path, domain)
+        response = asyncio.run(bot.get_response(user_message, sender_id))
+        return jsonify(response)
+    except Exception as e:
+        logger.exception(f"An error occurred: {str(e)}")
+        return jsonify({'error': 'An error occurred'})
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5055)
+    app.run(debug=False, host='0.0.0.0', port=5007)
 
