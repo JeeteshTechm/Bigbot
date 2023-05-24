0ed922dc073be9680627da1f00bf419b9a64377d
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-05-24 19:37:32 +0530

    Update adapter_api.py

--- rasa_app/adapter/adapter_api.py
+++ rasa_app/adapter/adapter_api.py
@@ -1,5 +1,7 @@
 import json
-from flask import Flask, jsonify, request
+import yaml
+import tempfile
+from flask import Flask, jsonify, request, send_file
 from bigbot_to_rasa3_adapter import JSONToYAMLConverter
 from rasa3_to_bigbot_adapter import YAMLToJsonConverter
 
@@ -8,24 +10,22 @@ app = Flask(__name__)
 @app.route('/json-to-yaml', methods=['POST'])
 def convert_json_to_yaml():
     try:
-        uploaded_file = request.files['file']
-        payload = json.load(uploaded_file)
+        payload = request.get_json()
         json_converter = JSONToYAMLConverter(payload)
         yaml_data = json_converter.convert_to_yaml()
-        return jsonify({'result': yaml_data})
+
+        with tempfile.NamedTemporaryFile(suffix='.yml', delete=False) as temp_file:
+            temp_file.write(yaml_data.encode('utf-8'))
+
+        temp_filename = temp_file.name
+
+        response = send_file(temp_filename, mimetype='text/yaml')
+        response.headers['Content-Disposition'] = 'attachment; filename=output.yml'
+
+        return response
     except Exception as e:
         return jsonify({'error': str(e)})
 
-@app.route('/yaml-to-json', methods=['POST'])
-def convert_yaml_to_json():
-    try:
-        uploaded_file = request.files['file']
-        payload = json.load(uploaded_file)
-        yaml_converter = YAMLToJsonConverter(payload)
-        json_data = yaml_converter.convert_to_json()
-        return jsonify({'result': json_data})
-    except Exception as e:
-        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5008)
