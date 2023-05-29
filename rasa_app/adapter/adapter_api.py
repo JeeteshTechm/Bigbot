d8e23da2769c7e6f94c77e9b233ddc81adb0acdf
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-05-29 12:51:42 +0530

    Update adapter_api.py

--- rasa_app/adapter/adapter_api.py
+++ rasa_app/adapter/adapter_api.py
@@ -1,9 +1,14 @@
 import json
 import yaml
 import tempfile
+import os
+import logging
 from flask import Flask, jsonify, request, send_file
 from bigbot_to_rasa3_adapter import JSONToYAMLConverter
 from rasa3_to_bigbot_adapter import YAMLToJsonConverter
+
+logging.basicConfig(level=logging.INFO, filename='logs/adapter.log')
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -11,6 +16,8 @@ app = Flask(__name__)
 def convert_json_to_yaml():
     try:
         payload = request.get_json()
+        logging.info('Received JSON payload: {}'.format(payload))
+
         json_converter = JSONToYAMLConverter(payload)
         yaml_data = json_converter.convert_to_yaml()
 
@@ -18,14 +25,42 @@ def convert_json_to_yaml():
             temp_file.write(yaml_data.encode('utf-8'))
 
         temp_filename = temp_file.name
+        logging.info('Converted JSON to YAML: {}'.format(yaml_data))
 
         response = send_file(temp_filename, mimetype='text/yaml')
         response.headers['Content-Disposition'] = 'attachment; filename=output.yml'
 
         return response
     except Exception as e:
+        logging.error('Error converting JSON to YAML: {}'.format(str(e)))
         return jsonify({'error': str(e)})
 
+@app.route('/yaml-to-json', methods=['POST'])
+def convert_yaml_to_json():
+    try:
+        file = request.files['file']
+        logging.info('Received YAML file: {}'.format(file.filename))
+
+        yaml_content = yaml.safe_load(file)
+        filename = file.filename
+        logging.info('Loaded YAML content: {}'.format(yaml_content))
+
+        yaml_converter = YAMLToJsonConverter(yaml_content)
+        json_data = yaml_converter.convert_to_json()
+
+        with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as temp_file:
+            temp_file.write(json_data.encode('utf-8'))
+
+        temp_filename = temp_file.name
+        logging.info('Converted YAML to JSON: {}'.format(json_data))
+
+        response = send_file(temp_filename, mimetype='text/json')
+        response.headers['Content-Disposition'] = 'attachment; filename=output.json'
+
+        return response
+    except Exception as e:
+        logging.error('Error converting YAML to JSON: {}'.format(str(e)))
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5008)
+    app.run(debug=False, host='0.0.0.0', port=5005)
