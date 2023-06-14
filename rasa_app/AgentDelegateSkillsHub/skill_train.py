4c77616f26cbbd6b0be6caad9a4ffb45474057c5
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-06-14 15:10:37 +0530

    Update skill_train.py

--- rasa_app/AgentDelegateSkillsHub/skill_train.py
+++ rasa_app/AgentDelegateSkillsHub/skill_train.py
@@ -1,9 +1,10 @@
 import logging
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from rasa_file_generator_test import RasaFileGenerator
-from create_delegate_utterances import JSONDataProcessor
+from skill_file_generator import RasaFileGenerator
+from skill_utterance_generator import JSONDataProcessor
 from actions.run_actions import ActionServerManager
+from availiable_skills import AvailiableSkills
 import json
 import rasa
 
@@ -11,13 +12,13 @@ app = Flask(__name__)
 CORS(app)
 
 logging.basicConfig(level=logging.INFO, filename='logs/skill_builder_train.log')
-logging.basicConfig(level=logging.INFO)
-
 
 def rasa_file_generator(payload):
     skill_id = str(payload["bot_id"])
     logging.info(f"Generating Rasa files for skill ID: {skill_id}")
-    
+
+    has_form = any("form" in intent for intent in payload["intents"])
+
     processor = JSONDataProcessor(payload)
     processor.create_delegate_utterances()
 
@@ -28,8 +29,12 @@ def rasa_file_generator(payload):
     rasa_project.generate_training_data()
     rasa_project.generate_domain_file()
     rasa_project.upload_config_file("input/config.yml")
-    rasa_project.generate_actions_file()
-    rasa_run_actions.start_action_server()
+
+    if has_form:
+        print("Yes, at least one intent has a form.")
+        rasa_project.generate_actions_file()
+        rasa_run_actions.start_action_server()
+
 
     logging.info("Rasa files generation completed")
 
@@ -52,6 +57,7 @@ def rasa_train(skill_id):
 def generate():
     try:
         payload = request.get_json()
+        payload["intents"]=[intent for intent in payload["intents"] if intent]
         skill_id = str(payload["bot_id"])
         rasa_file_generator(payload)
         rasa_train(skill_id)
@@ -61,6 +67,14 @@ def generate():
         logging.error(f"An error occurred: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
+@app.route('/availiable_skills', methods=['POST'])
+@cross_origin()
+def skills_data():
+    json_file_path = "input/skills.json"
+    reader = AvailiableSkills(json_file_path)
+    intent_names = reader.get_skill_names()
+    return intent_names
+    
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5006)
