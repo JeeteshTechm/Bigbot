214b3ac2a26cd2bf74c4abe393e211b09a578ab8
tsleesg <114339973+tsleesg@users.noreply.github.com>
2023-08-03 07:59:02 +0200

    Update dialogue_generator.py
    abstracted YAML reading into a separate read_yaml function
    added file and key validation
    generalise string handling
    use env variables
    use a more concise prompt from confluence
    remove unused imports
    refactor and code check with GPT

--- rasa_app/adapter/dialogue_generator.py
+++ rasa_app/adapter/dialogue_generator.py
@@ -1,9 +1,8 @@
-import openai
+import os
 import yaml
-from flask import Flask, jsonify, request
+from flask import Flask, request, send_file
 from flask_cors import CORS, cross_origin
-from flask import Flask, jsonify, request, send_file
-import requests
+import openai
 import tempfile
 from rasa3_to_bigbot_adapter import YAMLToJsonConverter
 from datetime import date
@@ -11,69 +10,69 @@ from datetime import date
 app = Flask(__name__)
 CORS(app)
 
-config_path = "config.yml"
-with open(config_path, 'r') as config_file:
-    config = yaml.safe_load(config_file)
+CONFIG_PATH = os.getenv("CONFIG_PATH", "config.yml")
+EXAMPLE_YAML = os.getenv("EXAMPLE_YAML", "yaml_shema.yml")
+MODEL = os.getenv("MODEL", "gpt-3.5-turbo")
 
-openai.api_key = config['endpoints']['api_url']
+def read_yaml(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"{file_path} not found.")
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
-example_yaml = "yaml_shema.yml"
-with open(example_yaml, 'r') as file:
-    example_yaml_data = yaml.safe_load(file)
+config = read_yaml(CONFIG_PATH)
+openai.api_key = config.get('endpoints', {}).get('api_url')
 
+if not openai.api_key:
+    raise ValueError("Missing OpenAI API key.")
+
+example_yaml_data = read_yaml(EXAMPLE_YAML)
 example_yaml_text = yaml.dump(example_yaml_data)
 
 def get_completion(prompt):
-    #model="gpt-4"
-    model="gpt-3.5-turbo"
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
-    model=model,
-    messages=messages,
-    temperature=0,
+        model=MODEL,
+        messages=messages,
+        temperature=0,
     )
-    return response.choices[0].message["content"]
+    content = response.choices[0].message.get("content")
+    if not content:
+        raise ValueError("No content in response.")
+    return content
 
-
-def convert_dates_to_strings(yaml_obj):
-    if isinstance(yaml_obj, list):
-        for i in range(len(yaml_obj)):
-            yaml_obj[i] = convert_dates_to_strings(yaml_obj[i])
-    elif isinstance(yaml_obj, dict):
-        for key, value in yaml_obj.items():
-            yaml_obj[key] = convert_dates_to_strings(value)
-    elif isinstance(yaml_obj, date):
-        return yaml_obj.isoformat()
-    return yaml_obj
-
+def replace_in_structure(data, old_value, new_value):
+    if isinstance(data, list):
+        return [replace_in_structure(item, old_value, new_value) for item in data]
+    elif isinstance(data, dict):
+        return {k: replace_in_structure(v, old_value, new_value) for k, v in data.items()}
+    elif isinstance(data, old_value):
+        return new_value(data)
+    return data
 
 @app.route('/data_generator', methods=['POST'])
 @cross_origin()
 def chat():
-    # try:
-        data = request.get_json()
-        user_message = data['message']
-        input= "Please generate rasa yaml with multiple intents for"+ user_message+ " similar to below schema with same structure like "+example_yaml_text
-        prompt = (input)
+    user_message = request.json['message']
+    input_text = f"Craft a dialogue for this user-described scenario - {user_message}. The dialogue must contain multiple intents and adhere strictly to this YAML schema:\n{example_yaml_text}. Use slots, forms, actions and other fields where appropriate."
+    response = get_completion(input_text)
 
-        response = get_completion(prompt)
-        print(response)
-        yaml_content = yaml.safe_load(response)
+    yaml_content = yaml.safe_load(response)
+    yaml_content = replace_in_structure(yaml_content, date, date.isoformat)
 
-        # Convert datetime.date objects to strings
-        yaml_content=convert_dates_to_strings(yaml_content)
+    yaml_converter = YAMLToJsonConverter(yaml_content)
+    json_data = yaml_converter.convert_to_json()
 
-        # Continue with JSON conversion
-        yaml_converter = YAMLToJsonConverter(yaml_content)
-        json_data = yaml_converter.convert_to_json()
-        with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as temp_file:
-            temp_file.write(json_data.encode('utf-8'))
+    with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as temp_file:
+        temp_file.write(json_data.encode('utf-8'))
 
-        temp_filename = temp_file.name
-        response = send_file(temp_filename, mimetype='text/json')
-        response.headers['Content-Disposition'] = 'attachment; filename=output.json'
-        
-        return (response)
+    temp_filename = temp_file.name
+    response = send_file(temp_filename, mimetype='text/json')
+    response.headers['Content-Disposition'] = 'attachment; filename=output.json'
+
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5003)
+    HOST = os.getenv("HOST", '0.0.0.0')
+    PORT = int(os.getenv("PORT", 5003))
+    app.run(debug=True, host=HOST, port=PORT)
