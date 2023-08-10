fd4f836f7a1551b867700058e6b7d34a6a4c363f
Bhavani915875 <121215222+Bhavani915875@users.noreply.github.com>
2023-08-10 16:55:06 +0530

    Update rasa3_to_bigbot_adapter.py with story and actions

--- rasa_app/adapter/rasa3_to_bigbot_adapter.py
+++ rasa_app/adapter/rasa3_to_bigbot_adapter.py
@@ -1,26 +1,40 @@
 import yaml
 import json
 import os
+import uuid  
 
 class YAMLToJsonConverter:
-    def __init__(self, yaml_file_path):
-        self.yaml_file_path = yaml_file_path
-        self.yaml_content = None
+    def __init__(self, payload):
+        self.yaml_content = payload
         self.json_data = {
+            "bot_id":str(uuid.uuid4()),
             "intents": []
         }
-
-    def load_yaml(self):
-        with open(self.yaml_file_path, 'r') as file:
-            self.yaml_content = yaml.safe_load(file)
-
     def process_intents(self):
+        intent_to_story_action_map = {}
+
+        # Populate the dictionary with the intent-to-story-action mapping
+        for story in self.yaml_content['stories']:
+            story_name = story['story']
+            for step in story['steps']:
+                if 'intent' in step:
+                    intent_name = step['intent']
+                    if intent_name not in intent_to_story_action_map:
+                        intent_to_story_action_map[intent_name] = {'story_name': story_name, 'actions': []}
+                if 'action' in step and step['action']:
+                    intent_to_story_action_map[intent_name]['actions'].append(step['action'])
+       
+        # Process intents and add the corresponding story_name and all actions from the story
         for intent_data in self.yaml_content['nlu']:
             intent_name = intent_data['intent']
+            
+            # Create the intent structure
             intent = {
-                "name": intent_name,
-                "responses": [],
-                "utterances": []
+                "intent": intent_name,
+                "response": [],
+                "utterances": [],
+                "story_name": intent_to_story_action_map.get(intent_name, {}).get('story_name', None),
+                "actions": intent_to_story_action_map.get(intent_name, {}).get('actions', [])
             }
 
             if intent_name in self.yaml_content['forms']:
@@ -42,7 +56,9 @@ class YAMLToJsonConverter:
                     utterance = utterance[1:].strip()
                 intent['utterances'].append(utterance)
 
+            # Append the intent to the JSON data
             self.json_data['intents'].append(intent)
+
         # Process stories to map intents with responses and add forms
         for story in self.yaml_content['stories']:
             for step in story['steps']:
@@ -56,14 +72,14 @@ class YAMLToJsonConverter:
                             for response in response_value:
                                 # Find the intent in the existing JSON structure and append the response
                                 for existing_intent in self.json_data['intents']:
-                                    if existing_intent['name'] == intent_name:
-                                        existing_intent['responses'].append(response['text'])
+                                    if existing_intent['intent'] == intent_name:
+                                        existing_intent['response'].append(response['text'])
                                         break
                         elif isinstance(response_value, dict):
                             # Find the intent in the existing JSON structure and append the response
                             for existing_intent in json_data['intents']:
-                                if existing_intent['name'] == intent_name:
-                                    existing_intent['responses'].append(response_value['text'])
+                                if existing_intent['intent'] == intent_name:
+                                    existing_intent['response'].append(response_value['text'])
                                     break
 
     def process_forms(self):
@@ -87,14 +103,14 @@ class YAMLToJsonConverter:
 
         for form, intent in form_intent_list:
             for intent_data in self.json_data['intents']:
-                if intent_data['name'] == intent:
+                if intent_data['intent'] == intent:
                     intent_data['form'] = form
                     intent_data['slots'] = []
 
                     for slot_name, slot_type in self.yaml_content['forms'][form].items():
                         slot = {
                             "name": slot_name,
-                            "type": slot_type[0]['type'] if isinstance(slot_type, list) else slot_type
+                            "type":"any"
                         }
 
                         if 'responses' in self.yaml_content and 'utter_ask_' + slot_name in self.yaml_content['responses']:
@@ -106,14 +122,13 @@ class YAMLToJsonConverter:
                         intent_data['slots'].append(slot)
                         intent_data['api_call'] = {
                                 "method": "",
-                                "URL": "",
+                                "url": "",
                                 "request_body": []
                             }
 
                     break
         for story in self.yaml_content['stories']:
             for step in story['steps']:
-                #print(step)
                 if 'slot_was_set' in step:
                     slot_was_set = step['slot_was_set']
                     for slot in slot_was_set:
@@ -122,8 +137,11 @@ class YAMLToJsonConverter:
                                 if "slots" in intent:
                                     for slot_info in intent['slots']:
                                         if slot_info['name'] == slot_name:
+                                            # Initialize 'examples' if it doesn't exist
+                                            if 'examples' not in slot_info:
+                                                slot_info['examples'] = []
                                             slot_info['examples'].append(slot_value)
-                                
+
 
     def add_ids_and_relations(self):
         intents = self.json_data['intents']
@@ -145,18 +163,18 @@ class YAMLToJsonConverter:
             intent['parent_id'] = intents[i-1]['id']
 
     def convert_to_json(self):
-        self.load_yaml()
         self.process_intents()
         self.process_forms()
         self.add_ids_and_relations()
 
         json_string = json.dumps(self.json_data, indent=4)
 
-        output_file = os.path.splitext(self.yaml_file_path)[0] + ".json"
+        return json_string
 
-        with open(output_file, "w") as file:
-            file.write(json_string)
+ 
+#with open("input/sindalah1.yml", 'r') as file:
+    #yaml_content = yaml.safe_load(file)
 
-    
-#yaml_converter = YAMLToJsonConverter("input/sindalah.yml")
+#yaml_converter = YAMLToJsonConverter(yaml_content)
 #json_data = yaml_converter.convert_to_json()
+#print(json_data)
